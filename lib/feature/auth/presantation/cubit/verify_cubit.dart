96ee914710bc723d4b50a30e67fdb1a7b3b2923e
deval_aveosoft <deval@aveosoftware.ca>
2021-04-28 10:28:39 +0530

    [No-Jira] verify dummy and redirect to dashboard

--- lib/feature/auth/presantation/cubit/verify_cubit.dart
+++ lib/feature/auth/presantation/cubit/verify_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:hagglex/core/error/failures/failure.dart';
 import 'package:hagglex/feature/auth/data/model/auth_user_model.dart';
@@ -19,13 +21,18 @@ class VerifyCubit extends Cubit<VerifyState> {
       return;
     }
     emit(VerifyProcessing());
-    final verifyFailedOrSuccess = await verify(verifyParams);
+
+
+    Timer(Duration(seconds: 2), () {
+      emit(Verified());
+    });
+    /*final verifyFailedOrSuccess = await verify(verifyParams);
     verifyFailedOrSuccess.fold(
       (l) {
         final failure = l as AuthFailure;
         emit(VerifyError(error: failure.error));
       },
       (r) => emit(Verified(authUserModel: r)),
-    );
+    );*/
   }
 }
